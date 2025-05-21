0c0e590915162e9b0357d5d994660b7a3402d3ed
prokimssam <86967542+prokimssam@users.noreply.github.com>
2025-05-21 11:37:36 +0900

    38. 씬전환

--- Assets/2_Scripts/CrashDetector.cs
+++ Assets/2_Scripts/CrashDetector.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
@@ -7,6 +8,7 @@ public class CrashDetector : MonoBehaviour
         if (other.CompareTag("Ground"))
         {
             Debug.Log("오! 내 머리야!");
+            SceneManager.LoadScene(0);
         }
     }
 }
