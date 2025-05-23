a58587a276a17bb35f4fa78502a725aa8481d391
prokimssam <86967542+prokimssam@users.noreply.github.com>
2025-05-23 15:20:07 +0900

    40번 까지 작업 완료

--- Assets/2_Scripts/CrashDetector.cs
+++ Assets/2_Scripts/CrashDetector.cs
@@ -3,12 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] private float reloadDelay = 1f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
         {
             Debug.Log("오! 내 머리야!");
-            SceneManager.LoadScene(0);
+            Invoke(nameof(ReloadScene), reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/2_Scripts/FinishLine.cs
+++ Assets/2_Scripts/FinishLine.cs
@@ -3,12 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private float reloadDelay = 2f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("완주했습니다!");
-            SceneManager.LoadScene(0);
+            Invoke(nameof(ReloadScene), reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
