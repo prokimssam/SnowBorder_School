2b159d4de345cbc1312cf2d974b06487662e40f7
baleunbit <vahyeon2514@sonline20.sen.go.kr>
2025-06-24 12:02:33 +0900

    Any Changes

---
+++ Assets/2_Scripts/AnyWay.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AnyWay : MonoBehaviour
+
+    
+{
+        void Start()
+    {
+        Debug.Log("This is a test script for Unity.");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
